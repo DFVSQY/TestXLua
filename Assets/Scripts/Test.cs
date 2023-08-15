add193fcc98d88f6a326764a109c887e55626c9d
DFVSQY <dfvsqy@qq.com>
2023-08-15 20:17:50 +0800

    一个简单的Hello World示例

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XLua;
 
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.LogFormat(name);
+        using LuaEnv env = new();
+        env.DoString("CS.UnityEngine.Debug.Log('hello world')");
     }
 }
