6e5d341d1f5d42e25727c172ad4075db849ec52f
DFVSQY <dfvsqy@qq.com>
2023-08-17 01:45:01 +0800

    自定义Loader加载Lua代码

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
 using XLua;
 
 public class Test : MonoBehaviour
 {
+    private readonly string luaDir = string.Format("{0}/Lua", Application.dataPath);
+
     // Start is called before the first frame update
     void Start()
     {
         using LuaEnv env = new();
-        env.DoString("CS.UnityEngine.Debug.Log('hello world')");
+        env.AddLoader(Loader);
+        env.DoString("require 'main'");
+    }
+
+    private byte[] Loader(ref string filePath)
+    {
+        string luaPath = string.Format("{0}/{1}.lua", luaDir, filePath);
+        if (File.Exists(luaPath))
+        {
+            byte[] bytes = File.ReadAllBytes(luaPath);
+            return bytes;
+        }
+        return null;
     }
 }
