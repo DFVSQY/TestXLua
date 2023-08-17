8a23f6e5f676655f46b28e53581e06b8edfdaa5e
DFVSQY <dfvsqy@qq.com>
2023-08-17 23:46:38 +0800

    在C#中用class/struct承接lua中的table

--- Assets/Lua/main.lua
+++ Assets/Lua/main.lua
@@ -3,3 +3,8 @@ print("start main lua scirpt!")
 _G.g_int_num = 10
 
 _G.g_float_num = 20.5
+
+_G.person_one = {
+    name = "Gates",
+    age = 15,
+}
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -9,6 +9,34 @@ public class Test : MonoBehaviour
 {
     private readonly string luaDir = string.Format("{0}/Lua", Application.dataPath);
 
+    /*
+    用Class承接lua中的table。
+
+    table的属性可以多于或者少于class的属性。可以嵌套其它复杂类型。 
+    要注意的是，这个过程是值拷贝，如果class比较复杂代价会比较大。
+    而且修改class的字段值不会同步到table，反过来也不会。
+    这个功能可以通过把类型加到GCOptimize生成降低开销。
+    */
+    private class PersonCls
+    {
+        public string name;
+        public int age;
+    }
+
+    /*
+    用Struct承接lua中的table。
+
+    table的属性可以多于或者少于struct的属性。可以嵌套其它复杂类型。 
+    要注意的是，这个过程是值拷贝，如果struct比较复杂代价会比较大。
+    而且修改struct的字段值不会同步到table，反过来也不会。
+    这个功能可以通过把类型加到GCOptimize生成降低开销。
+    */
+    private struct PersonSt
+    {
+        public string name;
+        public int age;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +47,12 @@ public class Test : MonoBehaviour
         int num1 = env.Global.Get<int>("g_int_num");
         double num2 = env.Global.Get<double>("g_float_num");
         Debug.LogFormat("num1:{0}, num2:{1}", num1, num2);
+
+        PersonCls personCls = env.Global.Get<PersonCls>("person_one");
+        Debug.LogFormat("person cls, name:{0}, age:{1}", personCls.name, personCls.age);
+
+        PersonSt personSt = env.Global.Get<PersonSt>("person_one");
+        Debug.LogFormat("person st, name:{0} age:{1}", personSt.name, personSt.age);
     }
 
     private byte[] Loader(ref string filePath)
